8cde481207d071cd79f4eca335970f5d6cc1222d
lniinikorpi <lassi.n94@gmail.com>
2019-04-20 17:16:32 +0300

    Added selects and saves to home controller

--- FoodApplication/Controllers/HomeController.cs
+++ FoodApplication/Controllers/HomeController.cs
@@ -9,12 +9,78 @@ namespace FoodApplication.Controllers
 {
     public class HomeController : Controller
     {
+        public int userID;
         public ActionResult Index()
         {
             FoodApplicationDBEntities entities = new FoodApplicationDBEntities();
-            List<Foods> model = entities.Foods.ToList();
-            entities.Dispose();
-            return View(model);
+            List<Users> user = entities.Users.ToList();
+            userID = user[0].UserID;
+            List<Foods> foods = entities.Foods.ToList();
+            List<FoodOfaDay> foodOfaDays = entities.FoodOfaDay.ToList();
+           /* List<WeekMenus> weekMenus = entities.WeekMenus.ToList();
+
+            var breakfasts = (from bf in foods
+                             where bf.TypeID == 1003
+                             select bf).ToList();
+            
+            var lunches = (from l in foods
+                          where l.TypeID == 1004
+                          select l).ToList();
+            for (int i = 0; i < 7; i++)
+            {
+                Random rnd = new Random();
+                int index = rnd.Next(0, (breakfasts.Count()));
+                var breakfast = breakfasts[index];
+                FoodOfaDay newFood = new FoodOfaDay();
+                newFood.FoodID = breakfast.FoodID;
+                DateTime today = DateTime.Now;
+                TimeSpan duration = new TimeSpan(i, 0, 0, 0);
+                DateTime newDate = today.Add(duration);
+                newFood.Date = newDate;
+                newFood.MenuID = 1000;
+                entities.FoodOfaDay.Add(newFood);
+                entities.SaveChanges();
+                
+            }
+            for (int i = 0; i < 14; i++)
+            {
+                Random rnd = new Random();
+                int index = rnd.Next(0, (lunches.Count()));
+                var lunch = lunches[index];
+                FoodOfaDay newFood = new FoodOfaDay();
+                newFood.FoodID = lunch.FoodID;
+                DateTime today = DateTime.Now;
+                TimeSpan duration = new TimeSpan(i, 0, 0, 0);
+                DateTime newDate = today.Add(duration);
+                newFood.Date = newDate;
+                newFood.MenuID = 1000;
+                entities.FoodOfaDay.Add(newFood);
+                entities.SaveChanges();
+
+            }
+            for (int i = 0; i < 7; i++)
+            {
+                Random rnd = new Random();
+                int index = rnd.Next(0, (breakfasts.Count()));
+                var breakfast = breakfasts[index];
+                FoodOfaDay newFood = new FoodOfaDay();
+                newFood.FoodID = breakfast.FoodID;
+                DateTime today = DateTime.Now;
+                TimeSpan duration = new TimeSpan(i, 0, 0, 0);
+                DateTime newDate = today.Add(duration);
+                newFood.Date = newDate;
+                newFood.MenuID = 1000;
+                entities.FoodOfaDay.Add(newFood);
+                entities.SaveChanges();
+
+            }*/
+
+            var weekFoods = from wf in foodOfaDays
+                            where wf.MenuID == 1000
+                            orderby wf.Date
+                            select wf;
+
+            return View(weekFoods);
         }
 
         public ActionResult About()
